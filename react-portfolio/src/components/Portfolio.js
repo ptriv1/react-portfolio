c04b7e58bdf7b9fc158ccd9220dc725716fb7b1a
Galatia <ptrivedius@aol.com>
2021-11-29 16:08:43 -0500

    pushing current code

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -5,23 +5,23 @@ function Portfolio() {
       <div class="portfolio">
           <h1>Portfolio</h1>
           <div class="project">
-            <h3>REBO</h3>
+            <h3 class="project-title">REBO</h3>
             <p>This is a real estate application.</p>
           </div>
           <div class="project">
-            <h3>Workout Tracker</h3>
+            <h3 class="project-title">Workout Tracker</h3>
           </div>
           <div class="project">
-            <h3>Regex Tutorial</h3>
+            <h3 class="project-title">Regex Tutorial</h3>
           </div>
           <div class="project">
-            <h3>Book Club Collective</h3>
+            <h3 class="project-title">Book Club Collective</h3>
           </div>
           <div class="project">
-            <h3>Employee Tracker</h3>
+            <h3 class="project-title">Employee Tracker</h3>
           </div>
           <div class="project">
-            <h3>Rotten Trailers</h3>
+            <h3 class="project-title">Rotten Trailers</h3>
           </div>
       </div>  
     )
