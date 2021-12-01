73a145f8162c3277d0403654f580d765aef3e438
Galatia <ptrivedius@aol.com>
2021-12-01 08:10:48 -0500

    editing contact form

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -7,18 +7,18 @@ function Contact() {
             <form class="contact-form">
                 <div class="contact-form-line">
                     <label class="contact-form-label" for="name">Name:</label>
-                    <input type="text" id="name" name="name"></input><br></br>
+                    <input class="contact-form-input" type="text" id="name" name="name"></input><br></br>
                 </div>
                 <div class="contact-form-line">
                     <label class="contact-form-label" for="email">Email:</label>
-                    <input type="text" id="email" name="email"></input><br></br> 
+                    <input class="contact-form-input" type="text" id="email" name="email"></input><br></br> 
                 </div>
                 <div class="contact-form-line">
                     <label class="contact-form-label" for="message">Message:</label>
-                    <input type="text" id="message" name="message"></input><br></br>
+                    <input class="contact-form-input" type="text" id="message" name="message"></input><br></br>
                 </div>
                 <div class="contact-form-line">
-                    <input type="submit" value="Submit"></input><br></br>
+                    <input class="contact-form-input" type="submit" value="Submit"></input><br></br>
                 </div>
             </form>
         </div>
