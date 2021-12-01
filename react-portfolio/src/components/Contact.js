b31fa5568729aa2ccaa362a47387274bdffbad44
Galatia <ptrivedius@aol.com>
2021-12-01 12:03:29 -0500

    edited contact form

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div class="contact">
-            <h1>Contact</h1>
-            <form class="contact-form">
+        <div>
+            <h1 class="contact-header">Contact</h1>
+            <p class="contact-paragraph">Please use the below form to contact me!</p>
+            <form action="action_page.php" class="contact-form">
                 <div class="contact-form-line">
                     <label class="contact-form-label" for="name">Name:</label>
                     <input class="contact-form-input" type="text" id="name" name="name"></input><br></br>
@@ -15,10 +16,10 @@ function Contact() {
                 </div>
                 <div class="contact-form-line">
                     <label class="contact-form-label" for="message">Message:</label>
-                    <input class="contact-form-input" type="text" id="message" name="message"></input><br></br>
+                    <textarea id="message" name="message"></textarea><br></br>
                 </div>
                 <div class="contact-form-line">
-                    <input class="contact-form-input" type="submit" value="Submit"></input><br></br>
+                    <input class="contact-form-submit" type="submit" value="Submit"></input><br></br>
                 </div>
             </form>
         </div>
