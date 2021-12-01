827e26cd59442d12c4c135845fd03bbb17a979de
Galatia <ptrivedius@aol.com>
2021-12-01 12:51:18 -0500

    pushing current code

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -5,23 +5,28 @@ function Contact() {
         <div>
             <h1 class="contact-header">Contact</h1>
             <p class="contact-paragraph">Please use the below form to contact me!</p>
-            <form action="action_page.php" class="contact-form">
-                <div class="contact-form-line">
-                    <label class="contact-form-label" for="name">Name:</label>
-                    <input class="contact-form-input" type="text" id="name" name="name"></input><br></br>
-                </div>
-                <div class="contact-form-line">
-                    <label class="contact-form-label" for="email">Email:</label>
-                    <input class="contact-form-input" type="text" id="email" name="email"></input><br></br> 
-                </div>
-                <div class="contact-form-line">
-                    <label class="contact-form-label" for="message">Message:</label>
-                    <textarea id="message" name="message"></textarea><br></br>
-                </div>
-                <div class="contact-form-line">
-                    <input class="contact-form-submit" type="submit" value="Submit"></input><br></br>
-                </div>
-            </form>
+            <div>
+                <form action="mailto:trivedp18@gmail.com" method="get" enctype="text/plain">
+                    <div>
+                        <label class="contact-form-label" for="name">Name:
+                        <input type="text" name="name" id="name" />
+                        </label>
+                    </div>
+                    <div>
+                        <label class="contact-form-label" for="email">Email:
+                        <input type="text" name="email" id="email" />
+                        </label>
+                    </div>
+                    <div>
+                        <label class="contact-form-label">Message:</label>
+                        <br />
+                        <textarea name="message"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" name="submit" value="Submit" />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
