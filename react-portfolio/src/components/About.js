97f3fe96f7238cd16801ab1d501bccd80199b680
Galatia <ptrivedius@aol.com>
2021-12-01 11:44:15 -0500

    edits to About Me section

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import dinnerPicture from '../dinner-picture.jpg';
 
 function About() {
   return (
@@ -6,11 +7,11 @@ function About() {
       <h1>About Me</h1>
       <div class="about-container">
         <div class="flex-left">
-          <img class="about-picture" src="react-portfolio\src\dinner-picture.jpg" alt="picture"/>
+          <img class="about-picture" src={dinnerPicture} alt="picture"/>
         </div>
         <div class="flex-right">
-          <p>Currently I work in customer service/software support and enjoy working with a wide variety of customers.  I'm also enrolled in a boot camp for web development through Georgia Tech where we learned CSS, HTML, JavaScript, Node.js, and React.</p>
-          <p>In my spare time I enjoy spending time with my family and reading fiction or nonfiction.</p>
+          <p class="about-paragraph">Currently I work in customer service/software support and enjoy working with a wide variety of customers.  I'm also enrolled in a boot camp for web development through Georgia Tech where we learned CSS, HTML, JavaScript, Node.js, and React.</p>
+          <p class="about-paragraph">In my spare time I enjoy spending time with my family and reading fiction or nonfiction.</p>
         </div>
       </div>
     </div>
